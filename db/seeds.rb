372f3886c6ab35ad82fe9de19faed51240c5bbe6
camilo ardila <miloardila98@gmail.com>
2022-09-18 18:23:57 -0500

    added controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+movies = {
+  'The Batman': 'themoviedb.org/t/p/w220_and_h330_face/mo7teil1qH0SxgLijnqeYP1Eb4w.jpg',
+  'Vértigo': 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/cOWPC7Sg9xoDM5i3uzs4iQtSPUj.jpg',
+  'Jurassic World: Dominion': 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vyTTjx1aHU9SOcEyF4kdcufjKqx.jpg',
+  'Top Gun: Maverick': 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AlWpEpQq0RgZIXVHAHZtFhEvRgd.jpg',
+  'Minions: El origen de Gru': 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zBk0guZ6NI2aHclb4sbrQdrrnOC.jpg'
+}
+
+movies.each do |key, value|
+  movie = Movie.create(
+    title: key,
+    overview: Faker::Movie.quote,
+    poster_url: value,
+    rating: rand(8.0..10.0).floor(1)
+  )
+  movie.save!
+end
